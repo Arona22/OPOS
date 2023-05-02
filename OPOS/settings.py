68f45b5d54fc7a2fcfa711f37a470f663ac5a6f8
arona22 <arona22@ru.is>
2023-05-02 13:04:38 +0000

    made html files for all three apps and connected them

--- OPOS/settings.py
+++ OPOS/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'OPOS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templatess'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
