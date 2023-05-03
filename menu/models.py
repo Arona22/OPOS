e98b07967da903319634006ff0b5a048f0796fd3
kristofer111 <kristofers21@ru.is>
2023-05-03 15:32:12 +0000

    Categories, Pizzas, PizzaImage, Sales, & Ratings models added

--- menu/models.py
+++ menu/models.py
@@ -11,29 +11,37 @@ class Pizzas(models.Model):
     description = models.CharField(max_length=255, blank=True)
     date_added = models.DateField()
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    price_small = models.IntegerField(default = 1999)
+    price_medium = models.IntegerField(default = 2999)
+    price_large = models.IntegerField(default = 3999)
 
     def __str__(self) -> str:
         return self.name
     
 
 class PizzaImage(models.Model):
-    pizza = models.ForeignKey(Pizzas, on_delete=models.CASCADE)
-    image = models.CharField(max_length=9999)
-
-
-class Prices(models.Model):
-    pizza = models.ForeignKey(Pizzas, on_delete=models.CASCADE)
-    small = models.FloatField()
-    medium = models.FloatField()
-    large = models.FloatField()
+    pizza = models.ForeignKey(
+        Pizzas, 
+        on_delete = models.CASCADE, 
+        default = ''
+        )
+    image = models.CharField(max_length=9999, blank=True)
 
 
 class Sales(models.Model):
-    pizza = models.ForeignKey(Pizzas, on_delete=models.CASCADE)
+    pizza = models.OneToOneField(
+        Pizzas, 
+        on_delete=models.CASCADE, 
+        primary_key = True,
+        )
     sales = models.FloatField()
 
 
 class Ratings(models.Model):
-    pizza = models.ForeignKey(Pizzas, on_delete=models.CASCADE)
+    pizza = models.OneToOneField(
+        Pizzas, 
+        on_delete=models.CASCADE, 
+        primary_key = True,
+        )
     rating = models.FloatField()
     
