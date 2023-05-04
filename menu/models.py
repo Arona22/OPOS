542adb23334264e104ea34edc8a5d72e0e2143f8
kristofer111 <kristofers21@ru.is>
2023-05-04 18:38:33 +0000

    ratings model added

--- menu/models.py
+++ menu/models.py
@@ -3,24 +3,31 @@ from django.db import models
 # Create your models here.
 
 class Categories(models.Model):
+
     name = models.CharField(max_length=255)
 
 
 class Pizzas(models.Model):
     name = models.CharField(max_length=255)
+    categories = models.ManyToManyField(Categories, related_name='pizzas')
     description = models.CharField(max_length=255, blank=True)
     date_added = models.DateField()
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
-    price_small = models.IntegerField(default = 1999)
-    price_medium = models.IntegerField(default = 2999)
-    price_large = models.IntegerField(default = 3999)
+    is_new = models.BooleanField(default=False)
+    price_small = models.IntegerField(default=1199)
+    price_medium = models.IntegerField(default=2199)
+    price_large = models.IntegerField(default=3199)
 
     def __str__(self) -> str:
         return self.name
-    
+
+
+class Pizza_category(models.Model):
+    pizza = models.ForeignKey(Pizzas, on_delete = models.CASCADE)
+    category = models.ForeignKey(Categories, on_delete = models.CASCADE)
+
 
 class PizzaImage(models.Model):
-    pizza = models.ForeignKey(
+    pizza = models.ForeignKey (
         Pizzas, 
         on_delete = models.CASCADE, 
         default = ''
@@ -29,19 +36,15 @@ class PizzaImage(models.Model):
 
 
 class Sales(models.Model):
-    pizza = models.OneToOneField(
+    pizza = models.OneToOneField (
         Pizzas, 
-        on_delete=models.CASCADE, 
         primary_key = True,
+        on_delete=models.CASCADE        
         )
-    sales = models.FloatField()
+    sales = models.IntegerField()
 
 
 class Ratings(models.Model):
-    pizza = models.OneToOneField(
-        Pizzas, 
-        on_delete=models.CASCADE, 
-        primary_key = True,
-        )
-    rating = models.FloatField()
+    name = models.CharField(max_length=255, primary_key=True, blank=True)
+    rating = models.IntegerField()
     
