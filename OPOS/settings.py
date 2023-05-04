cfb63ff3a904c6b37968b42d5bccb57ad731123f
arona22 <arona22@ru.is>
2023-05-04 12:45:08 +0000

    made 'pizza app' and 'edit profile app'

--- OPOS/settings.py
+++ OPOS/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
-    'static'
+    BASE_DIR / "static",
 ]
 
 # Default primary key field type
