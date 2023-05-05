634020b121c0292f9df75908c868b3aa958c0fb7
arona22 <arona22@ru.is>
2023-05-05 15:10:52 +0000

    edit profile

--- edit_profile/views.py
+++ edit_profile/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+people = {
+        "id": 0,
+        "name": "Aron",
+        "mail": "arona22@ru.is",
+        "phone": "898-3234",
+        "proimg": "img/profile_pic.jpg"
+    }
+#         {
+#         "id": 1,
+#         "name": "Lulli",
+#         "mail": "lulli@ru.is",
+#         "phone": "898-3894",
+#         "proimg": "img/marga_pizza.jpg"
+#     }
+# ]
 
 # Create your views here.
 def index(request):
-    return render(request, 'edit_profile/index.html')
+    return render(request, 'edit_profile/index.html', context={'people': people})
