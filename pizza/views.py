58a85877961d094c37c6a9ce4baef948f1646c70
Holmsteinn Egilsson <holmsteinn22@ru.is>
2023-05-05 15:36:44 +0000

    skdjskjkd

--- pizza/views.py
+++ pizza/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+import json
 
 
 # Create your views here.
 def index(request):
+    pizza_json = request.GET.get("pizza")
+    pizza = json.loads(pizza_json)
     return render(request, 'pizza/index.html')
-
