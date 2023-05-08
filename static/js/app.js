e208c979ea16b5d8c115e29aa9b02d31b053c268
arona22 <arona22@ru.is>
2023-05-08 13:13:02 +0000

    cart fixes

--- static/js/app.js
+++ static/js/app.js
@@ -25,14 +25,21 @@ const add_cart = (pizza) => {
 const displayCart = () => {
     let cart_list = document.getElementById("cart_list");
     cart_list.innerHTML='';
+    let price = 0
 
     let header = document.createElement("h2");
+    header.textContent = "CART"
+
+    cart_list.appendChild(header);
     
     for (let i = 0; i < cart.length; i++) {
         // PIZZAS
         let pizzaItem = document.createElement("li");
+
         let pizzaName = document.createElement("h4");
         let pizzaImg = document.createElement("img");
+        let pizzaPrice = document.createElement("h5")
+        let pizzaex = document.createElement("button")
 
         pizzaItem.id = "pizzaitem"
         pizzaName.textContent = cart[i].name;
@@ -40,11 +47,25 @@ const displayCart = () => {
         pizzaImg.src = "/static/" + cart[i].image;
         pizzaImg.id = "cart_img"
 
+        pizzaPrice.textContent = cart[i].price + "kr"
+
+        pizzaex.id = "pizzaex"
+
         pizzaItem.appendChild(pizzaName);
+        pizzaItem.appendChild(pizzaPrice);
         pizzaItem.appendChild(pizzaImg);
+        pizzaItem.appendChild(pizzaex);
 
         cart_list.appendChild(pizzaItem);
+
+        price += cart[i].price
     }
+
+    let footer_price = document.createElement("h3");
+    footer_price.textContent = "Total: " + price + "kr"
+
+    cart_list.appendChild(footer_price);
+
 }
 
 
