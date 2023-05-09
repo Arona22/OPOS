92134d4db4154d139f58b1f6ef559ea17228ea05
Holmsteinn Egilsson <holmsteinn22@ru.is>
2023-05-09 18:33:58 +0000

    reyna filter :(

--- static/js/pizzas.js
+++ static/js/pizzas.js
@@ -31,3 +31,33 @@ $(document).ready(function() {
         }
     })
 })
+
+
+$(document).ready(function() {
+    $('#flexCheckDefault').on('change', function(e) {
+        e.preventDefault();
+        var filter = $('#flexCheckDefault').next('label').text();
+        
+        $.ajax({
+            'url': '/menu?filter=' + filter,
+            'type': 'GET',
+            success: function(resp) {
+                var newHtml = resp.data.map(pizza => {
+                    return `<div class="well-pizza">
+                                <a href="/menu/${pizza.id}" class="btn btn-primary" style="width: 18rem; background: rgba(118, 116, 116, 0.7);; border: gray;">
+                                    <img src="/static/${pizza.firstImage }" class="card-img-top" alt="">
+                                    <div class="card-body">
+                                        <h5 class="pizza-title">${pizza.name}</h5>
+                                        <p class="pizza-description">${pizza.description}</p>
+                                    </div>
+                                </a>
+                            </div>`
+                })
+                $('#flexCheckDefault').html(newHtml.join(''));
+            },
+            error: function(xhr, status, error) {
+                console.error(error);
+            }
+        })
+    })
+})
