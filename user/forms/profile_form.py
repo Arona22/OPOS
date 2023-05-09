01a87230a8aa082a4e3654436338d81682640452
kristofer <kristofers21@ru.is>
2023-05-09 16:06:53 +0000

    extended profile

---
+++ user/forms/profile_form.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm, widgets
+from user.models import Profile
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        exclude = [ 'id', 'user' ]
+        widgets = {
+            'name': widgets.TextInput(attrs={ 'class': 'form-control' }),
+            'email': widgets.TextInput(attrs={ 'class': 'form-control'}),
+            'phone': widgets.TextInput(attrs={ 'class': 'form-control' }),
+            'profile_image': widgets.TextInput(attrs={ 'class': 'form-control' }),
+        }
--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True, default='')
+    email = models.CharField(max_length=255, unique=True, default='')
+    phone = models.CharField(max_length=255, default='')
+    profile_image = models.CharField(max_length=9999, default='')
