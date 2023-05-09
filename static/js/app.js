094e7c0cdc561163455f01a4294162f3e1e53695
arona22 <arona22@ru.is>
2023-05-09 18:23:34 +0000

    checkout fixes

--- static/js/app.js
+++ static/js/app.js
@@ -2,8 +2,6 @@
 //     const checkbox = document.getElementById("flexCheckDefault")
 // }
 
-// var cart = []
-// localStorage.clear();
 let cart = []
 
 const get_cart = () => {
@@ -28,11 +26,15 @@ const add_cart = (id, name, img) => {
         "price": selectedValue,
         "quantity": 1
     }
-    // if (cart.includes(pizza)) {
-        //     let found_pizza = cart.find(({name}) => name === pizza.name);
-        //     found_pizza.quantity += 1
-        // }
 
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].id == obj.id && cart[i].price == obj.price) {
+            cart[i].quantity += 1
+            localStorage.setItem('myArray', JSON.stringify(cart));
+            return
+        }
+        console.log(cart[i])
+    }
     cart.push(obj);
     localStorage.setItem('myArray', JSON.stringify(cart));
 }
@@ -120,7 +122,7 @@ const deletepizza = (id) => {
     }
     localStorage.setItem('myArray', JSON.stringify(cart));
     displayCart()
-    console.log("delete ein pizza")
+    console.log("delete one pizza")
 }
 
 const deletecart = () => {
@@ -130,4 +132,86 @@ const deletecart = () => {
     console.log("delete cart")
 }
 
+
+const checkout_cart = () => {
+    console.log(cart)
+    get_cart()
+    let panel = document.getElementById("panel");
+    panel.innerHTML='';
+
+    for (let i = 0; i < cart.length; i++) {
+        // PIZZAS
+
+        //make box
+        let checkItem = document.createElement("div");
+        checkItem.className = "form-group"
+
+
+        //make img div
+        let checkimgdiv = document.createElement("div");
+        checkimgdiv.className = "col-sm-3 col-xs-3"
+        checkItem.appendChild(checkimgdiv);
+
+        //make img
+        let checkimg = document.createElement("img");
+        checkimgdiv.className = "img-responsive"
+        checkimg.src =  "/static/" + cart[i].image;
+        checkimg.style = "width: 100px;"
+        checkimgdiv.appendChild(checkimg)
+
+
+        //make product div
+        let checkproddiv = document.createElement("div");
+        checkproddiv.className = "col-sm-6 col-xs-6"
+        checkItem.appendChild(checkproddiv)
+
+        //make product name
+        let checkprodname = document.createElement("div");
+        checkprodname.className = "col-xs-12"
+        checkprodname.textContent = cart[i].name
+        checkproddiv.appendChild(checkprodname)
+
+        //make product quantity
+        let checkprodquan = document.createElement("div");
+        checkprodquan.className = "col-xs-12"
+        checkprodquan.textContent = "Quantity:" + cart[i].quantity
+        checkproddiv.appendChild(checkprodquan)
+
+        //make price div
+        let checkpricediv = document.createElement("div");
+        checkpricediv.className = "col-sm-3 col-xs-3 text-right"
+        checkItem.appendChild(checkpricediv)
+
+        //make price
+        let checkprice = document.createElement("h6");
+        checkprice.textContent = (cart[i].price * cart[i].quantity) + "kr"
+        checkpricediv.appendChild(checkprice)
+
+
+
+        //add box to panel
+        panel.appendChild(checkItem);
+
+    }
+    //make spacer
+    let checkspace = document.createElement("div");
+    checkspace.className = "form-group"
+    panel.appendChild(checkspace);
+
+    //make order total form
+    let checktotalform = document.createElement("div");
+    checkspace.className = "form-group"
+    panel.appendChild(checktotalform);
+
+    //make order total div
+    let checktotaldiv = document.createElement("div");
+    checkspace.className = "col-xs-12"
+    checktotalform.appendChild(checktotaldiv);
+
+    //make order total
+    let checktotal = document.createElement("strong");
+    checktotal.textContent = "FORLOOPA HER"
+    checktotaldiv.appendChild(checktotal);
+
+}
 
