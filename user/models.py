84ecaed5bc049ea53de525e5e0ef217be00f5b64
kristofer <kristofers21@ru.is>
2023-05-10 17:03:17 +0000

    logo fix

--- user/models.py
+++ user/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255, unique=True, default='')
-    email = models.CharField(max_length=255, unique=True, default='')
+    name = models.CharField(max_length=255, blank=True)
+    email = models.CharField(max_length=255, blank=True)
     phone = models.CharField(max_length=255, blank=True)
     profile_image = models.CharField(max_length=9999, blank=True)
 
