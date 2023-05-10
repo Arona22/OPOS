4b41b9c590c91bcaf61695f1860d67784db1500a
arona22 <arona22@ru.is>
2023-05-10 16:39:38 +0000

    user

--- user/urls.py
+++ user/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('logout', LogoutView.as_view(next_page='login'), name='logout'),
     path('edit_profile', views.edit_profile, name='edit_profile'),
     path('profile_details', views.get_user, name='profile_details'),
+    path('show_profile', views.show_profile, name='show_profile'),
+    
 ]
